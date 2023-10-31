8abacfbe932caa1a02ebad4ede689fdd950860da
FedorovMykhailo <fedorov.mykhailo@gmail.com>
2023-10-31 22:31:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use('/api/auth/',authRouter)
+app.use('/api/users/',authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
